4baa48131efee75aa570ecc11ba2584178272c9e
prestontewell <preston@tewellphoto.com>
2019-02-19 11:02:05 -0600

    first commit

--- store_item.rb
+++ store_item.rb
@@ -1,4 +1,8 @@
 energy_drink_1 = {:size => 16, :flavor => "Grape", :name => "nausea", :cost => 2.35}
 
+energy_drink_2 = {:size => 24, :flavor => "Avocado", :name => "hurl", :cost => 3.25}
+
 
 p "#{energy_drink_1[:name]} is a #{energy_drink_1[:flavor.downcase]} flavored energy drink sold in #{energy_drink_1[:size]} ounce cans for $#{energy_drink_1[:cost]}."
+
+p "#{energy_drink_2[:name]} is a #{energy_drink_2[:flavor.downcase]} flavored energy drink sold in #{energy_drink_2[:size]} ounce cans for $#{energy_drink_2[:cost]}."
